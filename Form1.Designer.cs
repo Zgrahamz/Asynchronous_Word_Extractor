89b6f5de1c10de78d9decd8f6b7200f5bebd527b
zach_ <zach_@PABLO_K_LINGUIA>
2025-07-02 15:00:27 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Asynchronous_Word_Extractor
+{
+    partial class BookWordListerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bookBox2 = new System.Windows.Forms.GroupBox();
+            this.displayBook2 = new System.Windows.Forms.RichTextBox();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.browseBtn2 = new System.Windows.Forms.Button();
+            this.selectedBook2 = new System.Windows.Forms.TextBox();
+            this.bookLabel2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.bookLabel1 = new System.Windows.Forms.Label();
+            this.selectedBook1 = new System.Windows.Forms.TextBox();
+            this.browseBtn1 = new System.Windows.Forms.Button();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.bookBox1 = new System.Windows.Forms.GroupBox();
+            this.displayBook1 = new System.Windows.Forms.RichTextBox();
+            this.bookBox2.SuspendLayout();
+            this.bookBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bookBox2
+            // 
+            this.bookBox2.Controls.Add(this.displayBook2);
+            this.bookBox2.Controls.Add(this.exitBtn);
+            this.bookBox2.Controls.Add(this.browseBtn2);
+            this.bookBox2.Controls.Add(this.selectedBook2);
+            this.bookBox2.Controls.Add(this.bookLabel2);
+            this.bookBox2.Location = new System.Drawing.Point(722, 12);
+            this.bookBox2.Name = "bookBox2";
+            this.bookBox2.Size = new System.Drawing.Size(675, 752);
+            this.bookBox2.TabIndex = 1;
+            this.bookBox2.TabStop = false;
+            // 
+            // displayBook2
+            // 
+            this.displayBook2.Location = new System.Drawing.Point(75, 178);
+            this.displayBook2.Name = "displayBook2";
+            this.displayBook2.Size = new System.Drawing.Size(426, 536);
+            this.displayBook2.TabIndex = 5;
+            this.displayBook2.Text = "";
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.exitBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.exitBtn.Location = new System.Drawing.Point(6, 108);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(122, 35);
+            this.exitBtn.TabIndex = 4;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = false;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // browseBtn2
+            // 
+            this.browseBtn2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.browseBtn2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.browseBtn2.Location = new System.Drawing.Point(506, 54);
+            this.browseBtn2.Name = "browseBtn2";
+            this.browseBtn2.Size = new System.Drawing.Size(164, 32);
+            this.browseBtn2.TabIndex = 3;
+            this.browseBtn2.Text = "Browse";
+            this.browseBtn2.UseVisualStyleBackColor = false;
+            this.browseBtn2.Click += new System.EventHandler(this.browseBtn2_Click);
+            // 
+            // selectedBook2
+            // 
+            this.selectedBook2.Location = new System.Drawing.Point(75, 54);
+            this.selectedBook2.Name = "selectedBook2";
+            this.selectedBook2.Size = new System.Drawing.Size(426, 26);
+            this.selectedBook2.TabIndex = 2;
+            // 
+            // bookLabel2
+            // 
+            this.bookLabel2.AutoSize = true;
+            this.bookLabel2.Location = new System.Drawing.Point(6, 57);
+            this.bookLabel2.Name = "bookLabel2";
+            this.bookLabel2.Size = new System.Drawing.Size(59, 20);
+            this.bookLabel2.TabIndex = 0;
+            this.bookLabel2.Text = "Book 2";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // bookLabel1
+            // 
+            this.bookLabel1.AutoSize = true;
+            this.bookLabel1.Location = new System.Drawing.Point(6, 57);
+            this.bookLabel1.Name = "bookLabel1";
+            this.bookLabel1.Size = new System.Drawing.Size(59, 20);
+            this.bookLabel1.TabIndex = 0;
+            this.bookLabel1.Text = "Book 1";
+            // 
+            // selectedBook1
+            // 
+            this.selectedBook1.Location = new System.Drawing.Point(75, 54);
+            this.selectedBook1.Name = "selectedBook1";
+            this.selectedBook1.Size = new System.Drawing.Size(426, 26);
+            this.selectedBook1.TabIndex = 1;
+            // 
+            // browseBtn1
+            // 
+            this.browseBtn1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.browseBtn1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.browseBtn1.Location = new System.Drawing.Point(506, 54);
+            this.browseBtn1.Name = "browseBtn1";
+            this.browseBtn1.Size = new System.Drawing.Size(164, 32);
+            this.browseBtn1.TabIndex = 2;
+            this.browseBtn1.Text = "Browse";
+            this.browseBtn1.UseVisualStyleBackColor = false;
+            this.browseBtn1.Click += new System.EventHandler(this.browseBtn1_Click);
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.submitBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.submitBtn.Location = new System.Drawing.Point(548, 108);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(122, 35);
+            this.submitBtn.TabIndex = 3;
+            this.submitBtn.Text = "Submit";
+            this.submitBtn.UseVisualStyleBackColor = false;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // bookBox1
+            // 
+            this.bookBox1.Controls.Add(this.displayBook1);
+            this.bookBox1.Controls.Add(this.submitBtn);
+            this.bookBox1.Controls.Add(this.browseBtn1);
+            this.bookBox1.Controls.Add(this.selectedBook1);
+            this.bookBox1.Controls.Add(this.bookLabel1);
+            this.bookBox1.Location = new System.Drawing.Point(12, 12);
+            this.bookBox1.Name = "bookBox1";
+            this.bookBox1.Size = new System.Drawing.Size(675, 752);
+            this.bookBox1.TabIndex = 0;
+            this.bookBox1.TabStop = false;
+            // 
+            // displayBook1
+            // 
+            this.displayBook1.Location = new System.Drawing.Point(75, 178);
+            this.displayBook1.Name = "displayBook1";
+            this.displayBook1.Size = new System.Drawing.Size(426, 536);
+            this.displayBook1.TabIndex = 4;
+            this.displayBook1.Text = "";
+            // 
+            // BookWordListerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1414, 775);
+            this.Controls.Add(this.bookBox2);
+            this.Controls.Add(this.bookBox1);
+            this.Name = "BookWordListerForm";
+            this.Text = "Asynchronous Word Extractor";
+            this.bookBox2.ResumeLayout(false);
+            this.bookBox2.PerformLayout();
+            this.bookBox1.ResumeLayout(false);
+            this.bookBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox bookBox2;
+        private System.Windows.Forms.Label bookLabel2;
+        private System.Windows.Forms.TextBox selectedBook2;
+        private System.Windows.Forms.Button browseBtn2;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Label bookLabel1;
+        private System.Windows.Forms.TextBox selectedBook1;
+        private System.Windows.Forms.Button browseBtn1;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.GroupBox bookBox1;
+        private System.Windows.Forms.RichTextBox displayBook2;
+        private System.Windows.Forms.RichTextBox displayBook1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asynchronous_Word_Extractor
+{
+    public partial class BookWordListerForm : Form
+    {
+        public BookWordListerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e) => Close();
+        //Zach Graham
+        private async void submitBtn_Click(object sender, EventArgs e)
+        {
+            List<Task> tasks = new List<Task>();
+            var book1 = openFileDialog1.FileName;
+            var book2 = openFileDialog2.FileName;
+
+            var wordtask = ListBookWords(book1, book2);
+            tasks.Add(wordtask);
+
+            await Task.WhenAll(tasks);
+
+            submitBtn.Enabled = false;
+
+            if (string.IsNullOrWhiteSpace(selectedBook1.Text) || string.IsNullOrWhiteSpace(selectedBook2.Text))
+            {
+                MessageBox.Show("Please use the browse buttons to select books to display.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            } 
+            else
+            {
+                submitBtn.Enabled = true;
+            }
+        }
+        //Zach Graham
+        private async Task ListBookWords(string book1, string book2)
+        {
+            await Task.WhenAll(
+                ProcessBookAsync(book1, displayBook1),
+                ProcessBookAsync(book2, displayBook2)
+            );
+        }
+        //Zach Graham
+        private async Task ProcessBookAsync(string bookPath, RichTextBox displayBox)
+        {
+            if (string.IsNullOrWhiteSpace(bookPath)) return;
+
+            var lines = await Task.Run(() => System.IO.File.ReadAllLines(bookPath));
+            var noPunctLines = lines.Where(word => !string.IsNullOrWhiteSpace(word))
+                .AsParallel()
+                .Select(x => x.ToLower().Trim()
+                    .Replace(",", "").Replace(".", "").Replace(";", "").Replace(":", "")
+                    .Replace("!", "").Replace("?", "").Replace("\"", "").Replace("'", "")
+                    .Replace("(", "").Replace(")", ""));
+            var filteredLines = noPunctLines.SelectMany(word => word.Split(' '));
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            filteredLines.ToList().ForEach(word =>
+            {
+                if (wordCount.ContainsKey(word))
+                    wordCount[word]++;
+                else
+                    wordCount.Add(word, 1);
+            });
+
+            var cutoff = wordCount.Where(word => !string.IsNullOrWhiteSpace(word.Key))
+                .OrderByDescending(word => word.Value)
+                .Take(10);
+
+            displayBox.Clear();
+            foreach (var word in cutoff)
+            {
+                displayBox.AppendText($"{word.Key}: {word.Value}{Environment.NewLine}");
+            }
+        }
+
+        private void browseBtn1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                selectedBook1.Text = openFileDialog1.FileName.Split('\\').LastOrDefault();
+            }
+        }
+
+        private void browseBtn2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog2.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                selectedBook2.Text = openFileDialog2.FileName.Split('\\').LastOrDefault();
+            }
+        }
+    }
+}
